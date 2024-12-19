346a10d01cc89945dee3ffacb1d147fd6b12dc1b
manoj <manojarun4820@gmail.com>
2024-12-19 11:26:36 +0530

    Refactor UI components and styles; update dependencies in package.json. Enhanced layout and styling in App.css, improved ProductList and ProductManagementApp for better user experience and responsiveness

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import ProductManagementApp from "./ProductManagementApp"
- 
+import ProductManagementApp from "./ProductManagementApp";
+
 const ProductList = () => {
   return (
     <div>
-         <h1>Product Management</h1>
-      <table className="table d-table w-100 m-3">
+      <h1>Add Product</h1>
+      <table className="table d-table w-100 m-3" cellSpacing="10px">
         <thead className="d-table w-100">
-          <tr >
+          <tr>
+            <th className="border-0 bg-transparent"></th>
             <th className="bg-transparent border-0">Product</th>
             <th className="bg-transparent border-0">Discount</th>
+            <th className="border-0 bg-transparent"></th>
+
           </tr>
         </thead>
         <tbody className="d-table w-100">
@@ -19,6 +22,5 @@ const ProductList = () => {
     </div>
   );
 };
- 
 
 export default ProductList;
--- src/components/ProductManagementApp.js
+++ src/components/ProductManagementApp.js
@@ -4,6 +4,9 @@ import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import icon from "../images/6-dots.png"
+import edit from "../images/edit.png"
+import context from "react-bootstrap/esm/AccordionContext";
 
 // Configuration Constants
 const API_CONFIG = {
@@ -52,21 +55,26 @@ const DraggableProduct = ({
       {/* Main Product Row */}
       <tr
         ref={(node) => ref(drop(node))}
-        className="mb-2 d-table w-80 position-relative"
+        className="mb-2 d-table w-80 position-relative bg-transparent"
       >
-        {/* Product Title and Edit Button */}
-        <td className="text-nowrap d-flex product-title p-1 justify-content-between">
-          <span className="">
-            {index + 1}. {product.title}
+        <td className="text-nowrap px-2 me-1 shadow-none border-0 bg-transparent">
+          <span className="d-flex align-items-center p-2 ">
+            <img src={icon} alt="Product Icon" className="me-2" /> {index + 1}.
           </span>
-          <button className="btn py-0" onClick={() => openProductPicker(index)}>
-            <i className="fa-solid fa-pen"></i>
-          </button>
+        </td>
+        {/* Product Title and Edit Button */}
+        <td className="text-nowrap border-0 px-2 w-50 bg-transparent">
+          <div className="p-2 d-flex justify-content-between product-title"> <span className="product-text">
+            {product.title}
+          </span>
+            <button className="btn p-0 d-flex" onClick={() => openProductPicker(index)}>
+              <img src={edit} alt="Product Icon" className="me-2" />
+            </button></div>
         </td>
         {/* Discount Input Section */}
-        <td className="discount-input-wrapper py-0 border-0">
+        <td className="discount-input-wrapper px-2 border-0">
           {product.id !== "001" ? (
-            <div className="d-flex justify-content-end w-100">
+            <div className="w-100">
               <input className="w-30 discount-inputfeild p-1 border-0 me-3" />
               <select className="w-50 border-0 p-1 discount-select">
                 <option>%off</option>
@@ -85,11 +93,10 @@ const DraggableProduct = ({
         </td>
 
         {/* Variants Toggle Button */}
-        <div className="justify-content-end w-100 text-end varints-btn bg-transparent">
+        <div className={`justify-content-end w-100 text-end varints-btn bg-transparent ${product.id !== '001' ? '' : 'p-0'}`}>
           <button
-            className={`border-0 bg-transparent text-primary show-variants-btn  ${
-              product.id === "001" ? "d-none" : ""
-            }`}
+            className={`border-0 bg-transparent text-primary show-variants-btn  ${product.id === "001" ? "d-none" : ""
+              }`}
             variant="outline-info"
             onClick={() => setShowVariants(!showVariants)}
           >
@@ -108,9 +115,8 @@ const DraggableProduct = ({
         {/* Remove Product Button (Hidden if only one product) */}
         <Button
           variant=""
-          className={`me-2 position-absolute cancel-icon bg-transparent ${
-            lengthofItems <= 1 ? "d-none" : "d-block"
-          }`}
+          className={`me-2 position-absolute cancel-icon bg-transparent ${lengthofItems <= 1 ? "d-none" : "d-block"
+            }`}
           onClick={() => removeProduct(index)}
         >
           <i className="fa-solid fa-xmark"></i>
@@ -122,13 +128,21 @@ const DraggableProduct = ({
           <tr
             ref={(node) => ref(drop(node))}
             key={varIndex}
-            className="mb-3 d-table w-80 position-relative ms-4"
+            className="mb-3 d-table w-80 position-relative ms-4 border-0"
           >
-            <td className="bg-transparent text-nowrap w-50 variant-title">
-              {variant.title}
+            <td className="text-nowrap px-2 me-1 shadow-none border-0 bg-transparent">
+              <span className="d-flex align-items-center p-2 ">
+                <img src={icon} alt="Product Icon" className="me-2" /> {varIndex + 1}.
+              </span>
             </td>
-            <td className="bg-transparent w-50 p-0">
-              <div className="d-flex justify-content-end w-100">
+            <td className="bg-transparent text-nowrap w-50 variant-title border-0 bg-transparent">
+              <div className="p-2 d-flex justify-content-between product-title"> <span className="product-text">
+                {variant.title}
+              </span>
+              </div>
+            </td>
+            <td className="bg-transparent w-50 p-2 border-0 d-flex w-100">
+              <div className="w-100">
                 <input className="w-30 discount-inputfeild p-1 border-0 me-2" />
                 <select className="w-50 border-0 p-1 discount-select">
                   <option>%off</option>
@@ -250,7 +264,7 @@ function ProductManagementApp() {
     updateFilteredProducts(availableProducts);
   }, [searchTerm, availableProducts, updateFilteredProducts]);
 
-   // Handle scroll pagination
+  // Handle scroll pagination
   const handleScroll = useCallback(() => {
     const modalBody = modalBodyRef.current;
     if (!modalBody) return;
@@ -455,157 +469,178 @@ function ProductManagementApp() {
     console.log(`Number of selected variants: ${count}`);
   }, [selectedVariants, countSelectedVariants]);
 
+  // Throttle function
+  const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan;
+    return function (...args) {
+      if (!lastRan) {
+        func.apply(context, args);
+        lastRan = Date.now();
+      }
+      else {
+        clearTimeout(lastFunc);
+        setTimeout(() => {
+          if (Date.now() - lastRan >= limit) {
+            func.apply(context, args);
+            lastRan = Date.now()
+          }
+        }, limit - (Date.now() - lastRan));
+      }
+    }
+  };
+  // throttle the handleSeacrch function 
+  const throttleHandleSearch = useCallback(throttle(handleSearch, 300), [handleSearch]);
   return (
-    <>
-      <DndProvider backend={HTML5Backend}>
-        {/* Product List */}
-        {products.map((product, index) => (
-          <DraggableProduct
-            key={index}
-            product={product}
-            index={index}
-            moveProduct={moveProduct}
-            removeProduct={removeProduct}
-            openProductPicker={openProductPicker}
-            editingIndex={editingIndex}
-            removeProducts={removeProducts}
-            lengthofItems={products.length}
-          />
-        ))}
+    <DndProvider backend={HTML5Backend}>
+      {/* Product List */}
+      {products.map((product, index) => (
+        <DraggableProduct
+          key={index}
+          product={product}
+          index={index}
+          moveProduct={moveProduct}
+          removeProduct={removeProduct}
+          openProductPicker={openProductPicker}
+          editingIndex={editingIndex}
+          removeProducts={removeProducts}
+          lengthofItems={products.length}
+        />
+      ))}
 
-        {/* Add Product Button */}
-        <div className="w-80 d-flex justify-content-end ">
-          <Button className="addproduct-btn px-3" onClick={addEmptyProduct}>
-            Add Product
-          </Button>
-        </div>
+      {/* Add Product Button */}
+      <div className="w-80 d-flex justify-content-end ">
+        <Button className="addproduct-btn px-3" onClick={addEmptyProduct}>
+          Add Product
+        </Button>
+      </div>
 
-        {/* Product Picker Modal */}
-        <Modal
-          show={showProductPicker}
-          onHide={() => setShowProductPicker(false)}
+      {/* Product Picker Modal */}
+      <Modal
+        show={showProductPicker}
+        onHide={() => setShowProductPicker(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Select Products</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          ref={modalBodyRef}
+          onScroll={handleScroll}
+          style={{
+            maxHeight: "500px",
+            overflowY: "auto",
+          }}
         >
-          <Modal.Header closeButton>
-            <Modal.Title>Select Products</Modal.Title>
-          </Modal.Header>
-          <Modal.Body
-            ref={modalBodyRef}
-            onScroll={handleScroll}
-            style={{
-              maxHeight: "500px",
-              overflowY: "auto",
-            }}
-          >
-            <Form.Control
-              type="text"
-              placeholder="Search products"
-              value={searchTerm}
-              onChange={(e) => handleSearch(e.target.value)}
-              className="mb-3"
-            />
-            {filteredProducts.map((product) => {
-              const anyVariantSelected = product.variants.some(
-                (variant) => variant.selected
-              );
-              const allVariantsSelected = product.variants.every(
-                (variant) => variant.selected
-              );
+          <Form.Control
+            type="text"
+            placeholder="Search products"
+            value={searchTerm}
+            onChange={(e) => throttleHandleSearch(e.target.value)}
+            className="mb-3"
+          />
+          {filteredProducts.map((product) => {
+            const anyVariantSelected = product.variants.some(
+              (variant) => variant.selected
+            );
+            const allVariantsSelected = product.variants.every(
+              (variant) => variant.selected
+            );
 
-              return (
-                <Card key={product.id} className="mb-2 border-0">
-                  <Card.Body className="py-0">
-                    <Row>
-                      <div className="d-flex align-items-center mb-2">
-                        <input
-                          type="checkbox"
-                          id={`product-${product.id}`}
+            return (
+              <Card key={product.id} className="mb-2 border-0">
+                <Card.Body className="py-0">
+                  <Row>
+                    <div className="d-flex align-items-center mb-2">
+                      <input
+                        type="checkbox"
+                        id={`product-${product.id}`}
+                        className="me-2"
+                        checked={allVariantsSelected || anyVariantSelected}
+                        onChange={(e) => {
+                          const isChecked = e.target.checked;
+                          handleProductSelect(isChecked, product);
+                          product.variants.forEach((variant) => {
+                            variant.selected = isChecked;
+                          });
+                          setSelectedVariants(
+                            isChecked ? product.variants : []
+                          );
+                        }}
+                      />
+                      {product.image && (
+                        <img
+                          src={product.image}
+                          alt=""
                           className="me-2"
-                          checked={allVariantsSelected || anyVariantSelected}
-                          onChange={(e) => {
-                            const isChecked = e.target.checked;
-                            handleProductSelect(isChecked, product);
-                            product.variants.forEach((variant) => {
-                              variant.selected = isChecked;
-                            });
-                            setSelectedVariants(
-                              isChecked ? product.variants : []
-                            );
-                          }}
+                          style={{ maxWidth: "20px" }}
                         />
-                        {product.image && (
-                          <img
-                            src={product.image}
-                            alt=""
+                      )}
+                      <label htmlFor={`product-${product.id}`}>
+                        {product.title}
+                      </label>
+                    </div>
+                    {product.variants.map((variant) => (
+                      <div
+                        key={variant.id}
+                        className="d-flex align-items-center mb-2 justify-content-between ps-3"
+                      >
+                        <div style={{ display: "flex" }}>
+                          <input
+                            type="checkbox"
+                            id={`variant-${variant.id}`}
                             className="me-2"
-                            style={{ maxWidth: "20px" }}
+                            checked={variant.selected}
+                            onChange={(e) =>
+                              handleVariantSelect(
+                                e.target.checked,
+                                product,
+                                variant
+                              )
+                            }
                           />
-                        )}
-                        <label htmlFor={`product-${product.id}`}>
-                          {product.title}
+                          <div> {variant.title} </div>
+                        </div>
+                        <label htmlFor={`variant-${variant.id}`}>
+                          <div className="d-flex">
+                            {/* <div>{variant.avalible} Avalible</div> */}
+                            <div className="ps-3">${variant.price}</div>
+                          </div>
                         </label>
                       </div>
-                      {product.variants.map((variant) => (
-                        <div
-                          key={variant.id}
-                          className="d-flex align-items-center mb-2 justify-content-between ps-3"
-                        >
-                          <div style={{ display: "flex" }}>
-                            <input
-                              type="checkbox"
-                              id={`variant-${variant.id}`}
-                              className="me-2"
-                              checked={variant.selected}
-                              onChange={(e) =>
-                                handleVariantSelect(
-                                  e.target.checked,
-                                  product,
-                                  variant
-                                )
-                              }
-                            />
-                            <div> {variant.title} </div>
-                          </div>
-                          <label htmlFor={`variant-${variant.id}`}>
-                            <div className="d-flex">
-                              {/* <div>{variant.avalible} Avalible</div> */}
-                              <div className="ps-3">${variant.price}</div>
-                            </div>
-                          </label>
-                        </div>
-                      ))}
-                    </Row>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-            {/* Loading indicator */}
-            {loading && (
-              <div className="text-center my-3">
-                <span>Loading more products...</span>
-              </div>
-            )}
-
-            {/* End of results indicator */}
-            {!hasMore && availableProducts.length > 0 && (
-              <div className="text-center my-3">
-                <span>No more products to load</span>
-              </div>
-            )}
-          </Modal.Body>
-          <Modal.Footer className="justify-content-between">
-            <div>{selectedCount} product selected</div>
-            <div className="d-flex ">
-              <Button variant="secondary" onClick={handleCancel}>
-                Cancel
-              </Button>
-              <Button variant="success" className="ms-2" onClick={handleAdd}>
-                Add
-              </Button>
+                    ))}
+                  </Row>
+                </Card.Body>
+              </Card>
+            );
+          })}
+          {/* Loading indicator */}
+          {loading && (
+            <div className="text-center my-3">
+              <span>Loading more products...</span>
             </div>
-          </Modal.Footer>
-        </Modal>
-      </DndProvider>
-    </>
+          )}
+
+          {/* End of results indicator */}
+          {!hasMore && availableProducts.length > 0 && (
+            <div className="text-center my-3">
+              <span>No more products to load</span>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer className="justify-content-between">
+          <div>{selectedCount} product selected</div>
+          <div className="d-flex ">
+            <Button variant="secondary" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button variant="success" className="ms-2" onClick={handleAdd}>
+              Add
+            </Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    </DndProvider>
+
   );
 }
 
